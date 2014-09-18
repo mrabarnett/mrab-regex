f120f09140efcf0dd617c7c2f96a8c5ae32bbed1
MRAB <regex@mrabarnett.plus.com>
2014-09-18 01:05:51 +0100

    Adjusted line-endings in PKG-INFO.

--- PyPI/setup.py
+++ PyPI/setup.py
@@ -13,7 +13,7 @@ DOCS_DIR = os.path.join(BASE_DIR, 'docs')
 
 setup(
     name='regex',
-    version='2014.08.15',
+    version='2014.09.18',
     description='Alternative regular expression module, to replace re.',
     long_description=open(os.path.join(DOCS_DIR, 'Features.rst')).read(),
 
