ca9dca483b12d014cf7461418c2dc5384ae2897d
mrab <none@none>
2017-04-06 01:19:22 +0100

    Hg issue 236: Incorrect references to bugs.python.org issues

--- PyPI/setup.py
+++ PyPI/setup.py
@@ -16,7 +16,7 @@ DOCS_DIR = os.path.join(BASE_DIR, 'docs')
 
 setup(
     name='regex',
-    version='2017.02.08',
+    version='2017.04.05',
     description='Alternative regular expression module, to replace re.',
     long_description=open(os.path.join(DOCS_DIR, 'Features.rst')).read(),
 
