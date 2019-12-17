9bb16cf82ea5217f953af2dcd88e950f215086c8
mrab <none@none>
2019-12-17 21:17:10 +0000

    New release and upload because of problem with source distribution.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as file:
 
 setup(
     name='regex',
-    version='2019.12.17',
+    version='2019.12.18',
     description='Alternative regular expression module, to replace re.',
     long_description=long_description,
     author='Matthew Barnett',
