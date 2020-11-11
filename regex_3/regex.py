d5a5016c1b8090808eddeab98c5185dd63a13723
Matthew Barnett <parzilo@mrabarnett.plus.com>
2020-11-11 16:13:28 +0000

    Update version.

--- regex_3/regex.py
+++ regex_3/regex.py
@@ -241,7 +241,7 @@ __all__ = ["cache_all", "compile", "DEFAULT_VERSION", "escape", "findall",
   "VERSION1", "X", "VERBOSE", "W", "WORD", "error", "Regex", "__version__",
   "__doc__"]
 
-__version__ = "2.5.89"
+__version__ = "2.5.90"
 
 # --------------------------------------------------------------------
 # Public interface.
