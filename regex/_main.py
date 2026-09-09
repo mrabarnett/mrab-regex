7dd71c15c4fb5c94206bed1763abd4c2bd2f1b33
Matthew Barnett <github@mrabarnett.plus.com>
2026-09-09 20:47:29 +0100

    Fixed version.

--- regex/_main.py
+++ regex/_main.py
@@ -244,7 +244,7 @@ __all__ = ["cache_all", "compile", "DEFAULT_VERSION", "escape", "findall",
   "VERSION1", "X", "VERBOSE", "W", "WORD", "error", "Regex", "__version__",
   "__doc__", "RegexFlag"]
 
-__version__ = "2026.9.3"
+__version__ = "2026.9.10"
 
 # --------------------------------------------------------------------
 # Public interface.
